f9992f0ff9455e94ce468ff413e8df6b008d2f45
DigitaLeon <Saijin41@gmail.com>
2026-08-12 08:51:55 -0400

    Major updates to JS logic for random cards

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-let firstCard;
-let secondCard;
-let sum;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = '';
 
 const messageEl = document.getElementById('message-el');
@@ -10,10 +9,31 @@ const sumEl = document.getElementById('sum-el');
 const cardsEl = document.getElementById('cards-el');
 
 function startGame() {
-	firstCard = Math.floor(Math.random() * (11 - 2) + 2);
-	secondCard = Math.floor(Math.random() * (11 - 2) + 2);
+	isAlive = true;
+	let firstCard = getRandomCard();
+	let secondCard = getRandomCard();
 	sum = firstCard + secondCard;
-	cardsEl.textContent = `Cards: ${firstCard} ${secondCard}`;
+	cards = [firstCard, secondCard];
+	renderGame();
+}
+
+function getRandomCard() {
+	let rnd = Math.floor(Math.random() * 13 + 1);
+
+	if (rnd > 10) {
+		return 10;
+	} else if (rnd === 1) {
+		return 11;
+	} else {
+		return rnd;
+	}
+}
+
+function renderGame() {
+	cardsEl.textContent = 'Cards: ';
+	for (let i = 0; i < cards.length; i++) {
+		cardsEl.textContent += `${cards[i]} `;
+	}
 	sumEl.textContent = `Sum: ${sum}`;
 	if (sum <= 20) {
 		message = 'Do you want to draw a new card?';
@@ -25,12 +45,13 @@ function startGame() {
 		isAlive = false;
 	}
 	messageEl.textContent = message;
-	console.log(sum);
 }
 
 function newCard() {
-	console.log('Drawing a new card from the deck!');
-	let thirdCard = Math.floor(Math.random() * (11 - 2) + 2);
-	sum += thirdCard;
-	startGame();
+	if (isAlive && !hasBlackJack) {
+		let thirdCard = getRandomCard();
+		sum += thirdCard;
+		cards.push(thirdCard);
+		renderGame();
+	}
 }
