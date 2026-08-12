966cf9045bcce92cd279a2a0765939d90acdfcdd
DigitaLeon <Saijin41@gmail.com>
2026-08-11 21:41:51 -0400

    Added game felt image

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 let firstCard = Math.random() * (11 - 2) + 2;
 let secondCard = Math.random() * (11 - 2) + 2;
-let sum = firstCard + secondCard;
+let sum = Math.floor(firstCard + secondCard);
 
-console.log(sum);
+let hasBlackJack = false;
+let isAlive = true;
+
+let message = '';
+
+if (sum <= 20) {
+	message = 'Do you want to draw a new card? 🙂';
+} else if (sum === 21) {
+	message = "Wohoo! You've got Blackjack! 🥳";
+	hasBlackJack = true;
+} else {
+	message = "You're out of the game! 😭";
+	isAlive = false;
+}
+
+//CASH OUT
+console.log(hasBlackJack);
+console.log(isAlive);
