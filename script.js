e812783092f8cfb7f605388aef08ae08a353fdfa
DigitaLeon <Saijin41@gmail.com>
2026-08-11 22:55:39 -0400

    made changes to css and javascript

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-let firstCard = Math.random() * (11 - 2) + 2;
-let secondCard = Math.random() * (11 - 2) + 2;
-let sum = Math.floor(firstCard + secondCard);
-
+let firstCard;
+let secondCard;
+let sum;
 let hasBlackJack = false;
 let isAlive = true;
-
 let message = '';
 
-if (sum <= 20) {
-	message = 'Do you want to draw a new card? 🙂';
-} else if (sum === 21) {
-	message = "Wohoo! You've got Blackjack! 🥳";
-	hasBlackJack = true;
-} else {
-	message = "You're out of the game! 😭";
-	isAlive = false;
+const messageEl = document.getElementById('message-el');
+const sumEl = document.getElementById('sum-el');
+const cardsEl = document.getElementById('cards-el');
+
+function startGame() {
+	firstCard = Math.floor(Math.random() * (11 - 2) + 2);
+	secondCard = Math.floor(Math.random() * (11 - 2) + 2);
+	sum = firstCard + secondCard;
+	cardsEl.textContent = `Cards: ${firstCard} ${secondCard}`;
+	sumEl.textContent = `Sum: ${sum}`;
+	if (sum <= 20) {
+		message = 'Do you want to draw a new card?';
+	} else if (sum === 21) {
+		message = "You've got Blackjack!";
+		hasBlackJack = true;
+	} else {
+		message = "You're out of the game!";
+		isAlive = false;
+	}
+	messageEl.textContent = message;
+	console.log(sum);
 }
 
-//CASH OUT
-console.log(hasBlackJack);
-console.log(isAlive);
+function newCard() {
+	console.log('Drawing a new card from the deck!');
+	let thirdCard = Math.floor(Math.random() * (11 - 2) + 2);
+	sum += thirdCard;
+	startGame();
+}
